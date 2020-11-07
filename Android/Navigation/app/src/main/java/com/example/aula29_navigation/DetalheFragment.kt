9e9269196de41bd7dddf008b0af135a6c3cd339a
Giulia Machado <giuliamachado03@gmail.com>
2020-11-06 22:00:44 -0300

    AuA 29 - Navigation

---
+++ Android/Navigation/app/src/main/java/com/example/aula29_navigation/DetalheFragment.kt
@@ -0,0 +1,29 @@
+package com.example.aula29_navigation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.Navigation
+
+class DetalheFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_detalhe, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val navController = Navigation.findNavController(view)
+
+        view.findViewById<Button>(R.id.btnFragmentNome).setOnClickListener {
+            navController.navigate(R.id.nomeFragment)
+        }
+    }
+
+}
